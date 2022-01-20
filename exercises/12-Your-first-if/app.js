a25d4fd68042469d70164a0a785b7a3f85d719a2
Jose <josejesuscedeno@gmail.com>
2022-01-20 13:11:33 +0000

    prueba

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,25 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+function result() {
+
+    let result;
+    if (parseInt(total) > 100) {
+      result = 'We still have a bit of driving left to go';
+    } else {
+        if (parseInt(total) > 50) {
+            result = "We'll be there in 5 minutes";
+          } else {
+            if (parseInt(total) <= 50) {
+                result = "I'm parking. I'll see you right now";
+              } else 
+            result = 'NOT positive';
+          }
+      
+    }
+    return result;
+
+
+}
+console.log (result());
