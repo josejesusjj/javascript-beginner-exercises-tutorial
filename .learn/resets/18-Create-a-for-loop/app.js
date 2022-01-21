8d129a5b72b577de60e4aed9e7954b4ab9d3613e
Jose <josejesuscedeno@gmail.com>
2022-01-21 16:59:20 +0000

    hechos hasta ejercicio 18

--- .learn/resets/18-Create-a-for-loop/app.js
+++ .learn/resets/18-Create-a-for-loop/app.js
@@ -1,12 +1,7 @@
 /* This exercise should be moved before 17-Your first loop? */
 
 // Declare and write your function here:
-function standardsMaker(i) {
-    for (var i=0; i<300; i++){
-    console.log("I will write questions if I'm stuck")
-    }
-    return i
-}
+
 
 // Calling the function:
 standardsMaker();
