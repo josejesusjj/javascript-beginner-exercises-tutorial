4d5ee9f6197bc445eb1cb736d8fdb7da5dcf498b
Jose <josejesuscedeno@gmail.com>
2022-01-27 11:07:26 +0000

    falta solo el 24 objects

--- exercises/22-Russian-Roulette/app.js
+++ exercises/22-Russian-Roulette/app.js
@@ -1,17 +1,16 @@
-var bulletPosition = 4;
+let bulletPosition = 4;
 
-const spinChamber = () => {
+let spinChamber = () => {
 	var chamberPosition = Math.floor((Math.random() * 6) + 1);
 	return chamberPosition;
 };
 
-// Remove the // below and complete the commented lines
-const fireGun = (spinnerPosition) => {
-    if (spinChamber() == bulletPosition) return ("You're dead!");//el problema esta aqui, dentro del parentesis del if. no encuentro el match correcto
+let fireGun = (i) => {
+    
+    if (i == bulletPosition) return ("You're dead!")//el problema esta aqui, dentro del parentesis del if. no encuentro el match correcto
     else return ("Keep playing!");
+    
 };
 
 console.log(fireGun());
 
-console.log(spinChamber());
-console.log(bulletPosition);
